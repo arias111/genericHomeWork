a6771d6ff02e60b5a69e594de724bf8468ba4518
Nail Galiev <nail.galiev@waveaccess.ru>
2021-10-24 15:01:29 +0300

    [init] added base project

---
+++ Generics/Generics/Generics.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by user on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
